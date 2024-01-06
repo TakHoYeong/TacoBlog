98a46b7019d84894c464125ce6ac9b4d37a318c4
TakHoYeong <xkrghdud123@naver.com>
2024-01-06 21:06:56 +0900

    [추가 글 조회

--- src/main/java/me/taco/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/taco/springbootdeveloper/service/BlogService.java
@@ -6,14 +6,21 @@ import me.taco.springbootdeveloper.dto.AddArticleRequest;
 import me.taco.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
 
     private final BlogRepository blogRepository;
 
-    //블로그 글 추가 메서드
+    // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 글 조회 메서드
+    public List<Article> findAll(){
+        return blogRepository.findAll();
     }
 }
