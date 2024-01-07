3a9ce01091c73cd5e732d18a612b8475c0b3a421
TakHoYeong <xkrghdud123@naver.com>
2024-01-07 21:23:23 +0900

    [추가] BlogApiController findAllArticlicles 드

--- src/main/java/me/taco/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/taco/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.taco.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.taco.springbootdeveloper.domain.Article;
 import me.taco.springbootdeveloper.dto.AddArticleRequest;
+import me.taco.springbootdeveloper.dto.ArticleResponse;
 import me.taco.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,5 +27,16 @@ public class BlogApiController {
         //요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+
+    @GetMapping("api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
