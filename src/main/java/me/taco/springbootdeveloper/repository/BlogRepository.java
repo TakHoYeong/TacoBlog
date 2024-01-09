3788854372ccf94372cbb9b97c8eee8fb6fc990c
TakHoYeong <xkrghdud123@naver.com>
2024-01-09 20:42:01 +0900

    test 추가

--- src/main/java/me/taco/springbootdeveloper/repository/BlogRepository.java
+++ src/main/java/me/taco/springbootdeveloper/repository/BlogRepository.java
@@ -2,7 +2,9 @@ package me.taco.springbootdeveloper.repository;
 
 import me.taco.springbootdeveloper.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // 상속 받을 때 Article과 엔티티의 PK타입 Long을 인수로 넣는다.
+@Repository
 public interface BlogRepository extends JpaRepository<Article, Long> {
 }
---
+++ src/test/java/me/taco/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -0,0 +1,45 @@
+package me.taco.springbootdeveloper.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.taco.springbootdeveloper.dto.AddArticleRequest;
+import me.taco.springbootdeveloper.repository.BlogRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BlogApiControllerTest {
+
+    protected MockMvc mockMvc;
+
+    protected ObjectMapper objectMapper;
+
+    private WebApplicationContext context;
+
+    BlogRepository blogRepository;
+
+    @BeforeEach
+    public void mockMvcsetUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        blogRepository.deleteAll();
+    }
+
+    @DisplayName("addArticle: 글 추가")
+    @Test
+    public void addArticle() throws Exception{
+        //given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
+
+        //객체 JSON으로 직렬화
+    }
+}
