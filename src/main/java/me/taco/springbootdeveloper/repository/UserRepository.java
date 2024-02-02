0587a4b49b3ce3f8bc6bdc6e5fdb93b259ca58a1
TakHoYeong <xkrghdud123@naver.com>
2024-02-03 07:04:35 +0900

    create User repo

---
+++ src/main/java/me/taco/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.taco.springbootdeveloper.repository;
+
+import me.taco.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
