d818bf48e9b6a602d977901be06148b3addf7489
TakHoYeong <xkrghdud123@naver.com>
2024-02-05 07:23:39 +0900

    create Joindto

---
+++ src/main/java/me/taco/springbootdeveloper/dto/Join.java
@@ -0,0 +1,11 @@
+package me.taco.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Join {
+    private String email;
+    private String password;
+}
